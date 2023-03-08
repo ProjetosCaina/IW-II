a441caebcb9a04208e5e80d84cb4e9c193423469
Cainã <100374185+ProjetosCaina@users.noreply.github.com>
2023-03-08 16:42:47 -0300

    Add files via upload

---
+++ Modulos/calc.js
@@ -0,0 +1,9 @@
+const somaF = require ('./soma')
+const divF = require ('./div')
+const multF = require ('./mult')
+const subF = require ('./sub')
+
+console.log("a soma é: " + somaF(3,4))
+console.log("a soma é: " + divF(2,2))
+console.log("a soma é: " + multF(4,4))
+console.log("a soma é: " + subF(3,2))
